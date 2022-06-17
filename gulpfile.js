6857b85aff5de09e26aff390941efe28a827853e
Yurii <holub2015@ukr.net>
2022-06-17 12:55:38 +0300

    _mediaquery.sass added mixins for mediaquery

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ function scssTask(){
     return src(files.sassPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
         .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        //debug .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
     ); // put final CSS in dist folder
